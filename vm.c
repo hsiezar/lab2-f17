da62dcb3749b6a0590e32c6833f92a5367576253
Harley Siezar <hsiez001@sledge.cs.ucr.edu>
2018-05-24 23:11:12 -0700

    vm.c updated'

--- vm.c
+++ vm.c
@@ -335,7 +335,23 @@ copyuvm(pde_t *pgdir, uint sz)
     if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
       goto bad;
   }
+
+  for(i = USTACK - PGSIZE + 1; sizeStack > 0; i -= PGSIZE, sizeStack--){
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
+      goto bad;
+  }
   return d;
+
+
 
 bad:
   freevm(d);
